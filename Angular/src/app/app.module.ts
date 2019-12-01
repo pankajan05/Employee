b3334ba1ac7977a71f42f67eb5bfff76712a57a9
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-01 09:57:03 +0530

    create the models and services

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
