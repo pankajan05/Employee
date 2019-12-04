c78e76d62bd586c732b3e06de9d93ae6a5129647
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-04 18:23:49 +0530

    update

--- Node/index.js
+++ Node/index.js
@@ -7,7 +7,7 @@ var employeeController = require('./controllers/employeeController.js');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.listen(3000, () => console.log('Server started at port 3000.'));
 
